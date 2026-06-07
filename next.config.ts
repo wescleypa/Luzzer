736526904a23013b32a8ff32a55d6ecbae6275ae
Wescley Andrade <wecleypa.br@gmail.com>
2026-06-07 20:55:34 -0300

    fix: static export for cloudflare

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.3.2'],
+  output: 'export', // CF, remover dps
 };
 
 export default nextConfig;
